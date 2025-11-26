0e99f9f37ced3bedb0f6caa1a79d9dbf215067e4
Benji Bailey <benjaminbailey98@gmail.com>
2025-11-26 10:39:09 +0000

    Try fixing this error: `Build Error: Parsing ecmascript source code fail

--- src/app/artists/portal/page.tsx
+++ src/app/artists/portal/page.tsx
@@ -1,3 +1,4 @@
+
 'use client';
 
 import { useUser } from '@/firebase';
@@ -59,7 +60,7 @@ export default function ArtistPortalPage() {
             <div className="text-2xl font-bold">3</div>
             <p className="text-xs text-muted-foreground">in the next 30 days</p>
           </CardContent>
-        Card>
+        </Card>
         <Card>
           <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
             <CardTitle className="text-sm font-medium">New Requests</CardTitle>
