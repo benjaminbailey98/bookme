15ebe0d6bd54f0c6a34a67683695894442db30e2
Benji Bailey <benjaminbailey98@gmail.com>
2025-11-26 17:12:46 +0000

    Add (+) on each section (i.e. user, artists, venues, bookings, master ca

--- src/app/admin/bookings/page.tsx
+++ src/app/admin/bookings/page.tsx
@@ -1,4 +1,5 @@
 
+import { Button } from '@/components/ui/button';
 import {
     Card,
     CardContent,
@@ -6,12 +7,16 @@ import {
     CardHeader,
     CardTitle,
   } from '@/components/ui/card';
+import { PlusCircle } from 'lucide-react';
   
   export default function AdminBookingsPage() {
     return (
       <div className="flex-1 space-y-4 p-4 md:p-8 pt-6">
         <div className="flex items-center justify-between space-y-2">
           <h2 className="text-3xl font-bold tracking-tight">Manage Bookings</h2>
+           <Button>
+            <PlusCircle className="mr-2 h-4 w-4" /> Add Booking
+          </Button>
         </div>
         <Card>
           <CardHeader>
