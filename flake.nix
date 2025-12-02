33b0af40533442553b75e01298f9abb5351e5362
Benji Bailey <benjaminbailey98@gmail.com>
2025-12-02 04:15:28 +0000

    ERROR: failed to build: exit status 1 ERROR: failed to build: executing

--- flake.nix
+++ flake.nix
@@ -1,62 +1,38 @@
+
 {
   description = "A new Flake for a Node.js project";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    deploy-rs.url = "github:serokell/deploy-rs";
+    deploy-rs = {
+      url = "github:serokell/deploy-rs";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = { self, nixpkgs, flake-utils, deploy-rs }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = nixpkgs.legacyPackages.${system};
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
       in
       {
-        devShells.default = pkgs.mkShell {
-          buildInputs = [
-            pkgs.nodejs_22
-            self.packages.${system}.deploy
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nodejs
+            deploy-rs.packages.${system}.deploy-rs
           ];
         };
-
-        packages.deploy = deploy-rs.lib.${system}.mkDeploy {
-          # This assumes your Next.js app listens on port 3000 by default.
-          # The deployment environment will map its external port to this one.
-          ssh = {
-            user = "root";
-            host = "your-server-ip"; # This will be replaced by the deployment environment
-          };
-          
-          # This defines what to copy to the server.
-          # We're copying the whole project directory.
-          copy = {
-            sources = [ ./src ./public ./package.json ./package-lock.json ./next.config.ts ./tsconfig.json ];
-            files = [ ];
-          };
-
-          # This tells deploy-rs how to build and run the app.
-          profiles = {
-            "production" = {
-              user = "root";
-              path = deploy-rs.lib.${system}.activate.nix-profile;
-              script = ''
-                # Install dependencies
-                npm ci
-                
-                # Build the Next.js app
-                npx next build
-                
-                # Start the app
-                npx next start -p 3000
-              '';
-            };
-          };
-
-          magic = {
-            ssh = true;
-            copy = true;
-          };
+      }) // {
+        deploy.nodes.app = {
+            hostname = "app";
+            profiles.system.packages = with nixpkgs; [
+                nodejs
+            ];
         };
-      });
+        deploy.packages.x86_64-linux.app = deploy-rs.lib.x86_64-linux.activate.nixos self.deploy.nodes.app;
+    };
 }
