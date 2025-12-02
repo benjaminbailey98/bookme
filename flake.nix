f8b0bbe8e7fded7c0869866442d3c7e9372390dd
Benji Bailey <benjaminbailey98@gmail.com>
2025-12-02 00:55:08 +0000

    error: syntax error, unexpected ':', expecting '.' or '='        at /nix

--- flake.nix
+++ flake.nix
@@ -1,27 +1,22 @@
 {
-  "description": "A new Flake for a Node.js project",
-  "inputs": {
-    "nixpkgs": {
-      "url": "github:NixOS/nixpkgs/nixos-unstable"
-    },
-    "flake-utils": {
-      "url": "github:numtide/flake-utils"
-    }
-  },
-  "outputs": { self, nixpkgs, flake-utils }:
+  description = "A new Flake for a Node.js project";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs {
-          inherit system;
-        };
+        pkgs = nixpkgs.legacyPackages.${system};
       in
       {
-        devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            nodejs_20  # Use a recent Node.js version
-            # Add other system-level dependencies here if needed
+        devShell = pkgs.mkShell {
+          buildInputs = [
+            pkgs.nodejs
+            pkgs.deploy-rs
           ];
         };
-      }
-    )
+      });
 }
